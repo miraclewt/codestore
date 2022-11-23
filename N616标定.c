89c9b924a68cef983182f1fdd01143b7e1d71605
WT <58897879+miraclewt@users.noreply.github.com>
2022-11-23 11:26:07 +0800

    Add files via upload
    N616版本标定系数

---
+++ N616标定.c
@@ -0,0 +1,158 @@
+//°²È«Ô±
+#define ax_bias 0.005627888911719
+#define ay_bias -0.041719879329175
+#define az_bias -0.136342782016372
+#define gx_bias -0.718479933
+#define gy_bias -0.838843909
+#define gz_bias 0.816835501
+#define mx_bias -5.36125905145806
+#define my_bias -7.09003184965668
+#define mz_bias -4.99999932857333
+#define ax_k1 0.997975073236211
+#define ax_k2 0
+#define ax_k3 -2.377945669629839e-04
+#define ay_k1 1.650683475719821e-04
+#define ay_k2 0.999714194347401
+#define ay_k3 0.001508372189587
+#define az_k1 0
+#define az_k2 0
+#define az_k3 0.998603372998899
+#define mx_k1  1.01714509484200
+#define mx_k2  0
+#define mx_k3  -0.0258948746303265
+#define my_k1  -0.0131666849897886
+#define my_k2  0.979369619209160
+#define my_k3 -0.0263444008621741 
+#define mz_k1 0
+#define mz_k2 0
+#define mz_k3 1.00800541011014
+
+/*==============*/
+#define USER_ID_A 1
+#define USER_ID_B 0
+#define USER_ID_C 0
+#define USER_ID_D 0
+
+#if USER_ID_A
+
+#define ax_bias -0.0209294832031834
+#define ay_bias -0.0258771621846860
+#define az_bias -0.0608285500137568
+#define gx_bias -0.555475053
+#define gy_bias 0.181882439
+#define gz_bias 0.018141211
+#define mx_bias -12.5314956626775
+#define my_bias 3.61221348220407
+#define mz_bias 0.419489580800171
+#define ax_k1 1.00091473774105
+#define ax_k2 0
+#define ax_k3 -0.000624757285148941
+#define ay_k1 0.000944323290753944
+#define ay_k2 1.00090041763555
+#define ay_k3 0.00212443944626686
+#define az_k1 0
+#define az_k2 0
+#define az_k3 0.999865407569484
+#define mx_k1  1.24049794383921
+#define mx_k2  0
+#define mx_k3  -0.258698836691486
+#define my_k1 0.0237261646059451
+#define my_k2 1.21271809131804
+#define my_k3 -0.174644481562949
+#define mz_k1 0
+#define mz_k2 0
+#define mz_k3  1.25671421547020
+
+#endif
+
+#if USER_ID_B 
+
+#define ax_bias 0.00202236631508149
+#define ay_bias -0.0360450908379066
+#define az_bias -0.0211220994968246
+#define gx_bias -0.001242592
+#define gy_bias -0.481289517
+#define gz_bias 0.284562824
+#define mx_bias -0.347173124994114
+#define my_bias -4.48300887231483
+#define mz_bias 4.88505778689333
+#define ax_k1 0.999860785972101
+#define ax_k2 0
+#define ax_k3 0.00144326424926203
+#define ay_k1 -0.0000590307970710233
+#define ay_k2 1.00153704477136
+#define ay_k3 -0.000645366584193143
+#define az_k1 0
+#define az_k2 0
+#define az_k3 1.00004639205766
+#define mx_k1  1.54401091369679 
+#define mx_k2  0.160933994126168
+#define mx_k3  0
+#define my_k1 0
+#define my_k2 1.39797665991592
+#define my_k3 0
+#define mz_k1 -0.0960215133423158
+#define mz_k2 -0.149212338604546
+#define mz_k3  1.52544437239159
+
+#endif
+
+#if USER_ID_C
+#define ax_bias -0.00858302286879911
+#define ay_bias -0.0369110468298682
+#define az_bias -0.0619748247075495
+#define gx_bias -0.647433774
+#define gy_bias -1.231357166
+#define gz_bias  0.027106737
+#define mx_bias  -7.29240246921338
+#define my_bias  3.54210750961630
+#define mz_bias  13.9466114438635
+#define ax_k1 0.998572768529840
+#define ax_k2 0
+#define ax_k3 0.0000600045937163543
+#define ay_k1 -0.000166165438065612
+#define ay_k2 0.999962236118755
+#define ay_k3 0.00187409583285773
+#define az_k1 0
+#define az_k2 0
+#define az_k3 1.00024273723093 
+#define mx_k1  1.34448745072909
+#define mx_k2  0
+#define mx_k3  -0.236359609948853
+#define my_k1 0.00131039664426046
+#define my_k2 1.35395565782893
+#define my_k3 -0.0613512382337876
+#define mz_k1 0
+#define mz_k2 0
+#define mz_k3  1.36400337459148
+#endif
+
+#if USER_ID_D
+#define ax_bias -0.0347764794405027
+#define ay_bias -0.0452263460542648
+#define az_bias -0.303626968906597
+#define gx_bias -1.087409835
+#define gy_bias -1.371930828
+#define gz_bias 0.147863109
+#define mx_bias -10.1606680265352
+#define my_bias -23.2050953228561
+#define mz_bias 0.599176088349340
+#define ax_k1 0.999860785972101
+#define ax_k2 0
+#define ax_k3 0.00144326424926203
+#define ay_k1 -0.0000590307970710233
+#define ay_k2 1.00153704477136
+#define ay_k3 -0.000645366584193143
+#define az_k1 0
+#define az_k2 0
+#define az_k3 1.00004639205766
+#define mx_k1  1.56343544260200 
+#define mx_k2  0
+#define mx_k3  -0.216995944476108
+#define my_k1 0.0729120053524070
+#define my_k2 1.45521372864690 
+#define my_k3 -0.154698112963491
+#define mz_k1 0
+#define mz_k2 0
+#define mz_k3 1.62889544694466
+#endif
